e2b25fc3ef24abd7e83a58efa266b07567eff810
Brandon Trautmann <btrautmann@users.noreply.github.com>
2022-04-21 22:51:10 -0400

    Revert "throw errors on post"
    This reverts commit 2b370423fc1175d8f078a9bda29d139d3a5a1b5d.

--- src/slack-send.js
+++ src/slack-send.js
@@ -99,13 +99,7 @@ module.exports = async function slackSend(core) {
       }
 
       try {
-        await axios.post(webhookUrl, payload)
-          .then(function (response) {
-            throw new Error(response);
-          })
-          .catch(function (error) {
-            throw new Error(error);
-          });
+        await axios.post(webhookUrl, payload);
       } catch (err) {
         console.log('axios post failed, double check the payload being sent includes the keys Slack expects');
         console.log(payload);
